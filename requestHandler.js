d17a96bf527dbdde217300c5d35c71d63a03b925
Christian Schulze <christian.schulze@mni.thm.de>
2014-05-13 00:38:10 +0200

    sqlite3 preparation added

--- requestHandler.js
+++ requestHandler.js
@@ -1,14 +1,16 @@
 var url = require('url'); // parse GET arguments
 var config = require('config').Logging; // configuration
 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('sparkle-stats.db');
+
 function handleRequest(request) {
 	var parsedURL = url.parse(request.url, true); // true: get query as object
 	var arguments = JSON.stringify(parsedURL.query);
 
 	if (config.stdout != 1) {
 		// log to database
-		// TODO
-		// 
+
 	} else {
 		// log to stdout
 		if (arguments) {
